bbf0d84afafc4dd4d1a109506a46172799bc9c66
Srujana1910 <vallisrujana@gmail.com>
2025-06-04 11:07:41 +0530

    Create home_page.dart

---
+++ NavigationRouter/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'page_template.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageTemplate(
+      title: 'Home',
+      description: 'Welcome This is CHINTA VALLI SRUJANA '
+          'know me more.',
+    );
+  }
+}
