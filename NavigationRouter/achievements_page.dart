4b5d22918e6db927b2924ba2036ec0e39f601eee
Srujana1910 <vallisrujana@gmail.com>
2025-06-04 11:09:04 +0530

    Create achievements_page.dart

---
+++ NavigationRouter/achievements_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'page_template.dart';
+
+class AchievementsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageTemplate(
+      title: 'Achievements',
+      description: 'As a BTech students have Achievement 30+ certifications!',
+    );
+  }
+}
